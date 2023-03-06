9a744621bb9a337bb4a9d45040f6711e90581784
Keigo277 <thyeonex.8888@au.com>
2023-03-06 16:41:21 +0900

    タスクの削除機能とToggle機能の追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
-  tasks = [
-    "Jin",
-    "Airon man",
-    "Captain American",
-    "Thor",
-    "Ant Man"
+  tasks:Task[] = [
+   new Task( "Jin"),
+   new Task("Airon man") ,
+   new Task("Captain American") ,
+   new Task("Thor") ,
+   new Task("Ant Man") ,
 
   ]
   add(newTask:string){
-    this.tasks.push(newTask);
+    this.tasks.push(new Task(newTask));
   }
+  remove(exsitingTask:Task){
+   var userConfirmed = confirm(`${exsitingTask.title}は完全に削除されますがよろしいですか？`)
+   
+   if(userConfirmed){
+    this.tasks = this.tasks.filter(task => task !== exsitingTask);
+   }else{
+    return
+   }
+  }
+
+
+}
+
+class Task{
+  constructor(public title:string){
+
+  }
+  toggleIsDone(){
+    // alert('The task:"' + task + '"is done');
+    this.isDone = !this.isDone;
+  }
+  public isDone = false;
 }
